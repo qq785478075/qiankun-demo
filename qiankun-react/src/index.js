ce32aec2bf92ff5b923ead813b255242c043746a
qq785478075 <haley.wang@sinnet-cloud.cn>
2023-03-16 14:14:14 +0800

    react 报错修改

--- qiankun-react/src/index.js
+++ qiankun-react/src/index.js
@@ -1,11 +1,10 @@
 import "./public-path"
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-
 
 // if (window.__POWERED_BY_QIANKUN__) {
 //   // eslint-disable-next-line no-undef
@@ -14,13 +13,13 @@ import App from './App';
 
 function render(props) {
   const { container } = props;
-  ReactDOM.render(
+  const root = ReactDOM.createRoot(container ? container.querySelector('#root') : document.querySelector('#root'));
+  root.render(
     <React.StrictMode>
       <Router basename={window.__POWERED_BY_QIANKUN__ ? '/app-react' : '/'}>
         <App />
       </Router>
-    </React.StrictMode>,
-    container ? container.querySelector('#root') : document.querySelector('#root')
+    </React.StrictMode>
   );
 }
 
@@ -41,20 +40,3 @@ export async function unmount(props) {
   const { container } = props;
   ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'));
 }
-
-
-
-
-/* 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
- */
